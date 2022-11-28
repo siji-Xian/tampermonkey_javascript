e658c428aab901970aa17a32f07617fbded757f8
xian_siji <xiansiji777@163.com>
2022-11-28 11:29:22 +0800

    星图筛选-v0.2

--- 星图筛选条件导出.js
+++ 星图筛选条件导出.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         星图筛选条件
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        *://www.xingtu.cn/ad/creator/market
